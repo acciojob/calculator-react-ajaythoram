5279b5896448c478f0074f02f802059e1f2871a5
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-06-26 19:19:59 +0530

    Code updates

--- src/components/Cal.js
+++ src/components/Cal.js
@@ -29,23 +29,23 @@ const Cal = () => {
         <input type="text" value={str} onChange={handleInputChange} />
       </div>
       <div id="button">
-        <button id="btn1" onClick={()=> handleCbutton()}>C</button>
-        <button id="btn2" onClick={() => handleButtonClick("/")}>/</button>
-        <button id="btn3" onClick={() => handleButtonClick("*")}>*</button>
-        <button id="btn4" onClick={() => handleButtonClick("-")}>-</button>
-        <button id="btn5" onClick={() => handleButtonClick("7")}>7</button>
-        <button id="btn6" onClick={() => handleButtonClick("8")}>8</button>
-        <button id="btn7" onClick={() => handleButtonClick("9")}>9</button>
-        <button id="btn8" onClick={() => handleButtonClick("+")}>+</button>
-        <button id="btn9" onClick={() => handleButtonClick("4")}>4</button>
-        <button id="btn10" onClick={() => handleButtonClick("5")}>5</button>
-        <button id="btn11" onClick={() => handleButtonClick("6")}>6</button>
-        <button id="btn12" onClick={()=> handelButtonCal()}>=</button>
-        <button id="btn13" onClick={() => handleButtonClick("1")}>1</button>
-        <button id="btn14" onClick={() => handleButtonClick("2")}>2</button>
-        <button id="btn15" onClick={() => handleButtonClick("3")}>3</button>
-        <button id="btn16" onClick={() => handleButtonClick("0")}>0</button>
-        <button id="btn17" onClick={() => handleButtonClick(".")}>.</button>
+        <button id="btn-1" onClick={()=> handleCbutton()}>C</button>
+        <button id="btn-2" onClick={() => handleButtonClick("/")}>/</button>
+        <button id="btn-3" onClick={() => handleButtonClick("*")}>*</button>
+        <button id="btn-4" onClick={() => handleButtonClick("-")}>-</button>
+        <button id="btn-5" onClick={() => handleButtonClick("7")}>7</button>
+        <button id="btn-6" onClick={() => handleButtonClick("8")}>8</button>
+        <button id="btn-7" onClick={() => handleButtonClick("9")}>9</button>
+        <button id="btn-8" onClick={() => handleButtonClick("+")}>+</button>
+        <button id="btn-9" onClick={() => handleButtonClick("4")}>4</button>
+        <button id="btn-10" onClick={() => handleButtonClick("5")}>5</button>
+        <button id="btn-11" onClick={() => handleButtonClick("6")}>6</button>
+        <button id="btn-12" onClick={()=> handelButtonCal()}>=</button>
+        <button id="btn-13" onClick={() => handleButtonClick("1")}>1</button>
+        <button id="btn-14" onClick={() => handleButtonClick("2")}>2</button>
+        <button id="btn-15" onClick={() => handleButtonClick("3")}>3</button>
+        <button id="btn-16" onClick={() => handleButtonClick("0")}>0</button>
+        <button id="btn-17" onClick={() => handleButtonClick(".")}>.</button>
       </div>
     </div>
   );
